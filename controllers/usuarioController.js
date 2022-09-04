cff8f4e66a8aa3643ee04009a77195c9c4b9372c
EedgarHM <eedgar.hmarc@gmail.com>
2022-09-03 22:16:19 -0500

    creation layouts view and new endpoint to recover password

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,6 +1,6 @@
 const formularioLogin = (req, res)=> {
     res.render('auth/login',{
-        
+        pagina : 'Iniciar Sesion'
     })
 }
 
@@ -8,11 +8,22 @@ const formularioLogin = (req, res)=> {
 const formularioRegistro = (req, res) => {
     res.render('auth/registro',{
 
+        // Sending data to view
+        pagina : 'Crear Cuenta'
+    })
+}
+
+const formularioRecuperacionPassword = (req, res) => {
+    res.render('auth/recuperar-password',{
+
+        // Sending data to view
+        pagina : 'Recuperar Password'
     })
 }
 
 
 export {
     formularioLogin,
-    formularioRegistro
+    formularioRegistro,
+    formularioRecuperacionPassword
 }
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { formularioLogin,formularioRegistro } from '../controllers/usuarioController.js';
+import { 
+    formularioLogin,
+    formularioRegistro,
+    formularioRecuperacionPassword
+} from '../controllers/usuarioController.js';
 
 const router = express.Router();
 
 router.get('/login', formularioLogin)
 router.get('/registro',formularioRegistro)
+router.get('/recuperar-password',formularioRecuperacionPassword)
 
 export default router
